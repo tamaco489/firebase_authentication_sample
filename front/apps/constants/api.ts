30b218f7d126075ed0bfe1c3c36f8936cccfd07e
tamaco489 <tamaco489@gmail.com>
2025-03-15 20:46:31 +0900

    fix api constants

--- front/apps/constants/api.ts
+++ front/apps/constants/api.ts
@@ -1,4 +1,4 @@
-export const API_HOST = process.env.NEXT_PUBLIC_API_BASE_URL ?? '';
+export const API_HOST = process.env.API_BASE_URL ?? '';
 export const API_KEY = `${process.env.API_KEY ?? 'ABCDEFG123456789'}`;
 export const API_REQUEST_OPTIONS = {
   headers: {
