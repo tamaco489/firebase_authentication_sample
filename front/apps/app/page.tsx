cc06b500e6788a24c1239f76fba04cce73ebe756
tamaco489 <tamaco489@gmail.com>
2025-03-16 00:23:36 +0900

    fix page.tsx

--- front/apps/app/page.tsx
+++ front/apps/app/page.tsx
@@ -1,12 +1,41 @@
 'use client';
 
+import { useAuth } from '@/app/components/layout/providers/FirebaseAuth';
+import { useRouter } from 'next/navigation';
 import { FirebaseAuthProvider } from '@/app/components/layout/providers/FirebaseAuth';
+import { useState, useEffect } from 'react';
+import SignIn from '@/app/components/auth/SignIn';
 
 function Home() {
+  const { user, isLoading, isError } = useAuth();
+  const router = useRouter();
+  const [showSignIn, setShowSignIn] = useState(false);
+
+  useEffect(() => {
+    if (!isLoading && !user) {
+      // 未ログインの場合はSignInコンポーネントを表示
+      setShowSignIn(true);
+    } else {
+      setShowSignIn(false);
+    }
+  }, [user, isLoading, router]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error...</div>;
+  }
+
+  if (showSignIn) {
+    return <SignIn />;
+  }
+
   return (
     <div className="flex items-center justify-center h-screen">
       <main className="text-4xl font-bold">
-        Firebase Authentication sample by Next.js
+      {user ? `Welcome, ${user.displayName}!` : 'Firebase Authentication sample by Next.js'}
       </main>
       <footer className="">
       </footer>
