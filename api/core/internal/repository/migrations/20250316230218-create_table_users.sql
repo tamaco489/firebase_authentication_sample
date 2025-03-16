a392aa42635de2487d30ff96d6da8f7e6fdf4acb
tamaco489 <tamaco489@gmail.com>
2025-03-16 23:17:36 +0900

    fix migrate file

--- api/core/internal/repository/migrations/20250316230218-create_table_users.sql
+++ api/core/internal/repository/migrations/20250316230218-create_table_users.sql
@@ -1,17 +1,17 @@
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS `users` (
-    id VARCHAR(255) PRIMARY KEY COMMENT 'プロダクト固有のユニークなID',
-    username VARCHAR(255) NOT NULL COMMENT 'ゲーム内のアカウント名',
-    email VARCHAR(255) NOT NULL COMMENT 'ユーザのメールアドレス',
-    role ENUM('general', 'admin', 'beta_tester') NOT NULL COMMENT 'ユーザの権限レベル',
-    `status` ENUM('active', 'inactive', 'banned') NOT NULL COMMENT 'アカウントが有効か、無効か、強制退会済みかを判別',
-    last_login_at DATETIME COMMENT '最終ログイン日時',
-    created_at DATETIME NOT NULL COMMENT 'アカウント作成日時',
-    updated_at DATETIME NOT NULL COMMENT 'アカウント更新日時',
-    INDEX idx_email (email),
-    INDEX idx_role (role),
-    INDEX idx_status (`status`)
+  `id` VARCHAR(255) PRIMARY KEY COMMENT 'プロダクト固有のユニークなID',
+  `username` VARCHAR(255) NOT NULL COMMENT 'ゲーム内のアカウント名',
+  `email` VARCHAR(255) NOT NULL COMMENT 'ユーザのメールアドレス',
+  `role` ENUM('general', 'admin', 'beta_tester') NOT NULL COMMENT 'ユーザの権限レベル',
+  `status` ENUM('active', 'inactive', 'banned') NOT NULL COMMENT 'アカウントが有効か、無効か、強制退会済みかを判別',
+  `last_login_at` DATETIME COMMENT '最終ログイン日時',
+  `created_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  INDEX `idx_email` (`email`),
+  INDEX `idx_role` (`role`),
+  INDEX `idx_status` (`status`)
 );
 
 -- +migrate Down
