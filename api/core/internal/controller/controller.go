ad33e752a4f3e1fb556ae5abfa1660a542830fe9
tamaco489 <tamaco489@gmail.com>
2025-03-20 22:48:24 +0900

    fix controller di

--- api/core/internal/controller/controller.go
+++ api/core/internal/controller/controller.go
@@ -5,22 +5,23 @@ import (
 
 	"github.com/tamaco489/firebase_authentication_sample/api/core/internal/configuration"
 	"github.com/tamaco489/firebase_authentication_sample/api/core/internal/usecase"
+
+	repository_gen_sqlc "github.com/tamaco489/firebase_authentication_sample/api/core/internal/repository/gen_sqlc"
 )
 
 type Controllers struct {
 	config      configuration.Config
-	db          *sql.DB
 	userUseCase usecase.IUserUseCase
 }
 
 func NewCoreControllers(
 	cfg configuration.Config,
 	db *sql.DB,
+	queries repository_gen_sqlc.Queries,
 ) (*Controllers, error) {
-	userUseCase := usecase.NewUserUseCase()
+	userUseCase := usecase.NewUserUseCase(db, queries, db)
 	return &Controllers{
 		config:      cfg,
-		db:          db,
 		userUseCase: userUseCase,
 	}, nil
 }
