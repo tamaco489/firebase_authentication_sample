c3438d37a559817e5c7a4f3f699f53e3bf0e3089
tamaco489 <tamaco489@gmail.com>
2025-03-23 02:55:20 +0900

    fix middleware for use new firebase client and verify token

--- api/core/internal/middleware/authorization/jwt_auth.go
+++ api/core/internal/middleware/authorization/jwt_auth.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/tamaco489/firebase_authentication_sample/api/core/internal/configuration"
+	"github.com/tamaco489/firebase_authentication_sample/api/core/internal/library/firebase"
 )
 
 const healthCheckEndpoint = "/core/v1/healthcheck"
@@ -36,24 +38,44 @@ func JWTAuthMiddleware() gin.HandlerFunc {
 			return
 		}
 
-		// todo: 検証後削除
-		slog.InfoContext(c.Request.Context(), "success fetch id_token", slog.String("id_token", idToken))
+		// todo: 1. Firebase Admin SDK の初期化、及びインスタンス生成
+		firebaseClient, err := firebase.NewFirebaseClient(c.Request.Context(), configuration.Get().Firebase.GoogleServiceAccount)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": "Failed to initialize Firebase client"})
+			c.Abort()
+			return
+		}
 
-		// todo: 2. jwtの署名を検証（Firebase Admin SDKを使用する）
+		// todo: 2. jwtフォーマットの検証
+		token, err := firebaseClient.VerifyIDToken(c.Request.Context(), idToken)
+		if err != nil {
+			// トークン検証失敗
+			c.JSON(http.StatusUnauthorized, gin.H{"msg": "Invalid or expired token"})
+			c.Abort()
+			return
+		}
 
-		// todo: 3. jwtフォーマットの検証
+		// 検証が成功した場合、トークンから情報を取り出して後続の処理に利用
+		slog.InfoContext(c.Request.Context(), "ID token verified",
+			slog.String("sub", token.Subject),
+			slog.String("uid", token.UID),
+			slog.Int64("auth_time", token.AuthTime),
+			slog.Int64("expire", token.Expires),
+			slog.Int64("issued_at", token.IssuedAt),
+			slog.Any("firebase_info", token.Firebase),
+		)
 
-		// todo: 4. 検証済みのjwtを解析して、roleを確認
+		// todo: 3. 検証済みのjwtを解析して、roleを確認
 
-		// todo: 5. 検証済みのjwtを解析して、subを取得する
+		// todo: 4. 検証済みのjwtを解析して、subを取得する
 
-		// todo: 6. 取得したsubをkeyにしてredisからセッション情報を取得し、有効期限内かの判定を行う。
+		// todo: 5. 取得したsubをkeyにしてredisからセッション情報を取得し、有効期限内かの判定を行う。
 
-		// todo: 7. セッションが有効期限内: MySQLにアクセスせずに、セッションからuidを取得
+		// todo: 6. セッションが有効期限内: MySQLにアクセスせずに、セッションからuidを取得
 
-		// todo: 8. セッションが有効期限切れ: Redisに再度セッションを保存した上でMySQLにアクセスし、subをkeyにしてuidを取得
+		// todo: 7. セッションが有効期限切れ: Redisに再度セッションを保存した上でMySQLにアクセスし、subをkeyにしてuidを取得
 
-		// todo: 9. contextにuid、sub、roleを入れる
+		// todo: 8. contextにuid、sub、roleを入れる
 
 		c.Next()
 	}
